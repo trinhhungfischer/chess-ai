4abe3945a741059570040409d4066f7227ad3c91
trinhhungfischer <trinhhungfischer@gmail.com>
2022-04-30 22:01:34 +0700

    feat: Create new module AI

--- scripts/chess-game.py
+++ scripts/chess-game.py
@@ -1,3 +1,31 @@
-from chess_ai.Alphabeta import minimaxRoot
+import chess
+from scripts.chess_ai.ChessAI import ai_mapping
 
-print('Hello')
+ai_module = "Minimax"
+
+def game():
+    board = chess.Board()
+    n = 0
+    print(board)
+    while n < 100:
+      if n%2 == 0:
+          move = input("Enter move: ")
+          move = chess.Move.from_uci(str(move))
+          board.push(move)
+          if (len(list(board.legal_moves)) == 0):
+            if (board.is_checkmate() == True):
+              print("You win!!")
+            else:
+              print("Stale Mate")
+            break
+
+      else:
+          print("Computers Turn:")
+          move = ai_mapping[ai_module](3,board,True)
+          move = chess.Move.from_uci(str(move))
+          board.push(move)
+      print(board)
+      n += 1
+
+if __name__ == "__main__":
+  game()
