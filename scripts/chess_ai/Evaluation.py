90a25cbf1c2dd01f6e79591adf5deae1504982ce
trinhhungfischer <trinhhungfischer@gmail.com>
2022-05-05 11:45:18 +0700

    fix: Fix evaluation function

--- scripts/chess_ai/Evaluation.py
+++ scripts/chess_ai/Evaluation.py
@@ -1,13 +1,3 @@
-# def getPieceValue(piece):
-#   if(piece == None):
-#       return 0
-#   values = [10, 30, 30, 50, 90, 900]
-
-#   return values[chess_pieces.index(piece.upper())]
-
-from pickle import NONE
-
-
 def getPieceValue(piece):
   if(piece is None):
     return 0
@@ -25,12 +15,14 @@ def evaluation(board):
   i = 0
   evaluation = 0
   x = True
-  try:
-    x = bool(board.piece_at(i).color)
-  except AttributeError as e:
-    x = x
-  while i < 63:
-    i += 1
+  while i <= 63:
+    try:
+      x = bool(board.piece_at(i).color)
+    except AttributeError as e:
+      x = x
+
     evaluation = evaluation + (getPieceValue(str(board.piece_at(i)))
                                if x else -getPieceValue(str(board.piece_at(i))))
+    i += 1
+  
   return evaluation
